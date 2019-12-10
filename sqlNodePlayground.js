7bc013b5f58de6dbc8a783d0925a3e323252c754
Miguel Dias <tarantula.lan@gmail.com>
2019-12-10 03:54:43 -0300

    Update sqlNodePlayground.js

--- sqlNodePlayground.js
+++ sqlNodePlayground.js
@@ -19,6 +19,7 @@ const con = mysql.createConnection({
   host, user, password, database,
 });
 
+// Query tweets from Database
 const query = "SELECT * FROM tweets";
  
 // make to connection to the database.
@@ -34,6 +35,13 @@ con.connect(function(err) {
     console.log(result);
  });
 });
+
+// Insert Single Record
+var sql = "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("1 record inserted");
+  });
 
 // End Basic Connection and Query With Node to Mysql
 
